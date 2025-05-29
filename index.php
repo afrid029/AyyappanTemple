d5b11d6395ba055290e2feb8dc8dd3795a955687
afrid029 <mafrid029@gmail.com>
2025-05-30 00:05:23 +0400

    FirstCommit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php SESSION_START() ?>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="Assets/Images/logo.jpeg" />
+    <title>Aiyyapan koyil</title>
+    <meta charset="UTF-8">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet">
+   
+    <link rel="stylesheet" href="Assets/CSS/index.css">
+
+</head>
+
+<body>
+    <div class="banner">
+        <div class="banner-bg"></div>
+        <div class="banner-content">
+            <div class="banner-title">
+             Sri Hariharasudhan Ayyappan Temple
+            </div>
+            <div class="banner-logo">
+                <div class="banner-img"></div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ router.php
@@ -0,0 +1,43 @@
+<?php
+// Get the current URL path
+$request = $_SERVER['REQUEST_URI'];
+$request = explode('?', $request)[0];
+// print_r(explode('?', $request)[0]);
+
+// echo $request.implode('?');
+
+// echo $request;
+
+// exit();
+
+
+// Define routes
+switch ($request) {
+    case '/':
+        require 'index.php';  // Home page
+        break;
+    case '/loginpage':
+        require 'Pages/Login.php';  
+    break;
+    case '/login':
+        require 'Controllers/login.php';  
+    break;
+    case '/logout':
+        require 'Controllers/logoff.php';
+    break;
+    case '/dashboard':
+        require 'dashboard.php';
+        break;
+    case '/calendar':
+        require 'calendar.php';
+        break;
+    case '/gallery':
+        require 'gallery.php';
+        break;
+    default:
+        http_response_code(404); // Not Found
+        // require 'error.php';
+        break;
+}
+
+?>
